993f9baa3127c37d39908b27da21e4ce86ccd53d
Максим Семикрас <maksimsemikras@MacBook-Pro-Maksim.local>
2025-06-20 14:47:02 +0300

    sohranenie

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your JavaScript here.
+const button_1 = document.getElementById('button_follow') ; 
+
+const button_2 = document.getElementById('button_message'); 
+
+const message_window = document.getElementById('message_window')
+
+const button_close_window = document.getElementById('button_closed_window') 
+
+
+button_2.addEventListener('click', function(event) { 
+    message_window.style.display = 'flex' 
+})   
+
+button_close_window.addEventListener('click', function(event) { 
+    message_window.style.display = 'none' 
+})
